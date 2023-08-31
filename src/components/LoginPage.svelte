70a103af4e5e9e544f509fb5b4b4c7978fe3bf09
さくらみこ <lin@sz.cn.eu.org>
2023-08-31 08:38:53 +0300

    Update LoginPage.svelte

--- src/components/LoginPage.svelte
+++ src/components/LoginPage.svelte
@@ -1,5 +1,34 @@
 <script lang="ts">
-  
+  import express from 'express';
+
+const app = express();
+
+// Middleware to prevent direct access to files
+app.use('/users.txt', (req, res, next) => {
+  // Check if the request is coming from a trusted source
+  // You can implement your own logic here to determine the source of the request
+  const isTrustedSource = req.headers['referer'] === 'https://saikari.eu.org';
+
+  if (!isTrustedSource) {
+    // If the request is not from a trusted source, return an error or redirect
+    res.status(403).send('Access Forbidden');
+    // Alternatively, you can redirect the user to a different page
+    // res.redirect('/error');
+  } else {
+    // If the request is from a trusted source, proceed to serve the file
+    next();
+  }
+});
+
+// Serve the file
+app.get('/users.txt', (req, res) => {
+  // Read the file and send its content as the response
+  res.sendFile('users.txt');
+});
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
 
   let username = '';
   let password = '';
