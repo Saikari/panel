019aaa81d021202d542f6d8b09c5835b320422a4
さくらみこ <lin@sz.cn.eu.org>
2023-08-31 10:02:07 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,12 @@
 import App from './App.svelte';
-import axios from 'axios';
+import { createApp } from 'vue';
+import router from './router';
 
 const app = new App({
   target: document.querySelector('#app')
 });
+app.use(router);
+app.mount('#app');
+
 
 export default app;
-
-export const serverMiddleware = {
-  // API middleware
-  '/api/login': {
-    async handler(req, res) {
-      const { username, password } = req.body;
-
-      try {
-        const response = await axios.post('/api/login', { username, password });
-        console.log(response.data);
-        // Redirect to the next page or perform other actions
-        res.status(200).send('Login successful!');
-      } catch (error) {
-        console.log(error.message);
-        // Handle the error
-        res.status(401).send('Invalid login credentials. Please try again.');
-      }
-    }
-  }
-};
